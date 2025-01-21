4ea7ead2f29576166aaf6d5bdcb8e53cec059c1c
Suhyeon Lee <78308684+limeorange@users.noreply.github.com>
2025-01-22 00:22:01 +0900

    [Silver III] Title: 바이러스, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,27 +1,38 @@
-def dfs(idx):
-    global graph, visited
+# 250122 수 AM 12:21
+# graph: 빈 리스트 활용 ver.
+
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+def DFS(idx):
+    global graph, visited, answer
+
+    # 기본 1) 재방문 방지
     visited[idx] = True
 
-    for i in range(1, N+1):
-        if not visited[i] and graph[idx][i]:
-            dfs(i)
+    # 기본 2) 방문할 노드 탐색
+    for i in graph[idx]:
+        # 방문한 적이 없고, 연결된 노드라면
+        if not visited[i]:
+            answer += 1
+            DFS(i)
 
-# 0. 입력 및 초기화
-N = int(input()) # 컴퓨터 수
-M = int(input()) # 연결 수
-
-# 연결 정보 & 방문 정보 저장
-graph = [[False] * (N+1) for _ in range(N+1)]
+# 0. 입력 정보 받기
+N = int(input())
+M = int(input())
+graph = [[] for _ in range(N+1)]
 visited = [False] * (N+1)
+answer = 0
 
 # 1. graph에 연결 정보 채우기
 for _ in range(M):
     x, y = map(int, input().split())
-    graph[x][y] = True
-    graph[y][x] = True
+    graph[x].append(y)
+    graph[y].append(x)
 
-# 2. dfs(재귀함수) 호출
-dfs(1)
+# 2. DFS 실행
+DFS(1)
 
-# 3. 출력
-print(sum(visited)-1)
+# 3. 정답 출력 
+print(answer)
