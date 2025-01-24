0dc8b8a7ae7b0b707252cd74e49ef19ec3a70a9a
Suhyeon Lee <78308684+limeorange@users.noreply.github.com>
2025-01-25 02:23:20 +0900

    [Silver II] Title: 침투, Time: 752 ms, Memory: 159700 KB -BaekjoonHub

---
+++ 백준/Silver/13565. 침투/침투.py
@@ -0,0 +1,43 @@
+# 250125 토 AM 2:20
+
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+dy = [-1, 1, 0, 0]
+dx = [0, 0, -1, 1]
+
+def dfs(y, x):
+    global visited, map_, answer, N
+    visited[y][x] = True
+
+    if y == N:
+        answer = True
+        return
+
+    for i in range(4):
+        newY = y + dy[i]
+        newX = x + dx[i]
+        if map_[newY][newX] and not visited[newY][newX]:
+            dfs(newY, newX)
+
+# 0. 입력 및 초기화
+N, M = map(int, input().split())
+MAX = 1000 + 10
+map_ = [[False] * MAX for _ in range(MAX)]
+visited = [[False] * MAX for _ in range(MAX)]
+
+# 1. map에 연결 정보 채우기
+for i in range(1, N+1):
+    row = input()
+    for j in range(1, M+1):
+        map_[i][j] = (row[j-1] == "0")
+
+# 2. DFS 호출
+answer = False
+for j in range(1, M+1):
+    if map_[1][j]:
+        dfs(1, j)
+
+# 3. 정답 출력
+print("YES" if answer else "NO")
