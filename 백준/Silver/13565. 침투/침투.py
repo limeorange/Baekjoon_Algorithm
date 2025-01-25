29f677dbc10ae2cdca6263a1195b3fa16ed00c0a
Suhyeon Lee <78308684+limeorange@users.noreply.github.com>
2025-01-25 21:43:01 +0900

    [Silver II] Title: 침투, Time: 664 ms, Memory: 151768 KB -BaekjoonHub

--- 백준/Silver/13565. 침투/침투.py
+++ 백준/Silver/13565. 침투/침투.py
@@ -1,43 +1,45 @@
-# 250125 토 AM 2:29
-# visited 없는 버전 (map이 visited 역할 수행)
+# 250125 토 PM 9:41 => visited 없는 ver.
 
 import sys
-sys.setrecursionlimit(10**6)
+sys.setrecursionlimit(10**8)
 input = sys.stdin.readline
 
-dy = [-1, 1, 0, 0]
-dx = [0, 0, -1, 1]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-def dfs(y, x):
-    global map_, answer, N
+def DFS(y, x):
+    global map_, answer
+    
+    # 재방문 방지
     map_[y][x] = False
 
-    if y == N:
+    if y == M:
         answer = True
         return
 
+    # 상하좌우 방문할 노드 탐색
     for i in range(4):
-        newY = y + dy[i]
         newX = x + dx[i]
+        newY = y + dy[i]
         if map_[newY][newX]:
-            dfs(newY, newX)
+            DFS(newY, newX)
 
 # 0. 입력 및 초기화
-N, M = map(int, input().split())
+M, N = map(int, input().split())
 MAX = 1000 + 10
 map_ = [[False] * MAX for _ in range(MAX)]
+answer = False
 
 # 1. map에 연결 정보 채우기
-for i in range(1, N+1):
+for y in range(1, M+1):
     row = input()
-    for j in range(1, M+1):
-        map_[i][j] = (row[j-1] == "0")
+    for x in range(1, N+1):
+        map_[y][x] = (row[x-1] == '0')
 
 # 2. DFS 호출
-answer = False
-for j in range(1, M+1):
-    if map_[1][j]:
-        dfs(1, j)
+for i in range(1, N+1):
+    if map_[1][i]:
+        DFS(1, i)
 
 # 3. 정답 출력
 print("YES" if answer else "NO")
