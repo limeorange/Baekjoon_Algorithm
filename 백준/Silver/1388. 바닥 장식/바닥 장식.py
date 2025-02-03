cc714b039ac0a47a67c9c65c54ad0227161118f2
Suhyeon Lee <78308684+limeorange@users.noreply.github.com>
2025-02-03 16:40:24 +0900

    [Silver IV] Title: 바닥 장식, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1388. 바닥 장식/바닥 장식.py
+++ 백준/Silver/1388. 바닥 장식/바닥 장식.py
@@ -1,39 +1,41 @@
-# 250131 금 PM 5:54 => map, visited 활용
+# 250203 월 PM 4:39 (20m)
 
 import sys
 sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
 def DFS(y, x):
-    global map_
+    global map_, visited
 
     # 재방문 방지
     visited[y][x] = True
 
     # 방문할 노드 탐색
-    if map_[y][x] == '-' and map_[y][x+1] == '-':
+    if map_[y][x] == '-' and map_[y][x+1] == '-' and not visited[y][x+1]:
         DFS(y, x+1)
-    elif map_[y][x] == '|' and map_[y+1][x] == '|':
+
+    elif map_[y][x] == '|' and map_[y+1][x] == '|' and not visited[y+1][x]:
         DFS(y+1, x)
 
-# 0. 입력 및 초기화
+# 1. 입력 및 초기화
 N, M = map(int, input().split())
-map_ = [[''] * (M+10) for _ in range(N+10)]
-visited = [[False] * (M+10) for _ in range(N+10)]
+MAX = 50 + 10
+map_ = [[''] * MAX for _ in range(MAX)]
+visited = [[False] * MAX for _ in range(MAX)]
 answer = 0
 
-# 1. map에 연결 정보 채우기
+# 2. map에 연결 정보 채우기
 for i in range(1, N+1):
-    line = input()
+    line = input().strip()
     for j in range(1, M+1):
         map_[i][j] = line[j-1]
 
-# 2. DFS 호출
+# 3. DFS 호출
 for i in range(1, N+1):
     for j in range(1, M+1):
         if not visited[i][j]:
             DFS(i, j)
             answer += 1
 
-# 3. 정답 출력
+# 4. 정답 출력
 print(answer)
