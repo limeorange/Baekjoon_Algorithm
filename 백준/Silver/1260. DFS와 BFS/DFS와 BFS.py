0022976c40ab6355f3cfe05315d47e064d9b71cf
Suhyeon Lee <78308684+limeorange@users.noreply.github.com>
2025-02-03 18:08:23 +0900

    [Silver II] Title: DFS와 BFS, Time: 44 ms, Memory: 33544 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,47 +1,58 @@
-# 250123 목 AM 1:28
+# 250203 월 PM 6:07
 
 import sys
 sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
-def dfs(idx):
-    visited[idx] = True
-    print(idx, end = ' ')
+def DFS(V):
+    global graph, visited
 
-    for i in range(1, N+1):
-        if not visited[i] and graph[idx][i]:
-            dfs(i)
-
-def bfs():
-    queue = []
-
-    queue.append(V)
+    # 재방문 방지
     visited[V] = True
 
-    while queue:
-        idx = queue.pop(0)
-        print(idx, end = ' ')
+    print(V, end = ' ')
 
-        for i in range(1, N+1):
-            if not visited[i] and graph[idx][i]:
-                queue.append(i)
+    # 방문할 노드 탐색
+    for i in graph[V]:
+        if not visited[i]:
+            DFS(i)
+
+def BFS(V):
+    global graph, visited
+
+    # queue 활용 (FIFO)
+    q = []  
+    q.append(V)
+    visited[V] = True
+
+    while q:
+        node = q.pop(0)
+        print(node, end = ' ')
+        for i in graph[node]:
+            if not visited[i]:
+                q.append(i)
                 visited[i] = True
 
 # 0. 입력 및 초기화
 N, M, V = map(int, input().split())
-graph = [[False] * (N+1) for _ in range(N+1)]
-visited = [False] * (N+1)
+MAX = N+1
+graph = [[] * MAX for _ in range(MAX)]
+visited = [False] * MAX
 
 # 1. graph에 연결 정보 채우기
 for _ in range(M):
-    x, y = map(int, input().split())
-    graph[x][y] = True
-    graph[y][x] = True
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
 
-# 2. dfs
-dfs(V)
+# 오름차순 정렬
+for i in range(1, N+1):
+    graph[i] = sorted(graph[i])
+
+# 2. DFS 호출
+DFS(V)
+
+# 3. BFS 호출
 print()
-
-# 3. bfs
-visited = [False] * (N+1)
-bfs()
+visited = [False] * MAX
+BFS(V)
