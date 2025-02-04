107bfa3464b59e745eac0728c3c177488173cea5
Suhyeon Lee <78308684+limeorange@users.noreply.github.com>
2025-02-04 20:15:42 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 1092 ms, Memory: 61384 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
@@ -0,0 +1,45 @@
+# 250204 화 PM 8:15
+
+import sys
+input = sys.stdin.readline
+
+def BFS(R):
+    global graph, visited, answer
+    
+    q = []
+    q.append(R)
+    visited[R] = True
+    cnt = 0
+
+    while q:
+        cur = q.pop(0)
+        cnt += 1
+        answer[cur] = cnt
+        # 현재 노드에 인접한 오름차순 노드 중 방문하지 않은 노드 방문 => 너비 우선 탐색
+        for i in graph[cur]:
+            if not visited[i]:
+                visited[i] = True
+                q.append(i)
+
+# 0. 입력 및 초기화
+N, M, R = map(int, input().split())
+graph = [[] for _ in range(N+1)]
+visited = [False] * (N+1)
+answer = [0] * (N+1)
+
+# 1. graph에 연결 정보 채우기
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+# 인접 노드 오름차순 정렬
+for i in range(1, N+1):
+    graph[i].sort()
+
+# 2. BFS 호출
+BFS(R)
+
+# 3. 정답 출력
+for i in range(1, N+1):
+    print(answer[i])
