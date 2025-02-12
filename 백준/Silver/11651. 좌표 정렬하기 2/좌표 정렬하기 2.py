378461e658fe56585c8827dcade6cd7dacb5ccef
Suhyeon Lee <78308684+limeorange@users.noreply.github.com>
2025-02-12 19:32:42 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 320 ms, Memory: 46504 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
@@ -1,13 +1,13 @@
+# 250212 수 PM 7:32
+
 import sys
 input = sys.stdin.readline
 
 N = int(input())
 lst = []
-
 for _ in range(N):
-    x, y = map(int, input().split())
+    x, y = list(map(int, input().split()))
     lst.append((y, x))
-
 lst.sort()
 
 for i in lst:
