0410843c3b49718930b32801b2e822deb0a62418
Suhyeon Lee <78308684+limeorange@users.noreply.github.com>
2025-02-19 01:23:26 +0900

    [Silver IV] Title: 괄호, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -0,0 +1,19 @@
+# 250219 수 AM 1:18
+
+import sys
+# input = sys.stdin.readline
+
+T = int(input())
+answer = []
+for _ in range(T):
+    string = list(input())
+    stack = []
+    for i in range(len(string)):
+        if stack and stack[-1] == '(' and string[i] == ')':
+            stack.pop()
+        else:
+            stack.append(string[i])
+    answer.append('YES' if len(stack) == 0 else 'NO')
+
+for i in answer:
+    print(i)
