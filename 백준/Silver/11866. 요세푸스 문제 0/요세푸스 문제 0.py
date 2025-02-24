75edbdac5621f7801ed8cb748459d6b8b5763571
Suhyeon Lee <78308684+limeorange@users.noreply.github.com>
2025-02-24 18:23:40 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 56 ms, Memory: 34900 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -1,11 +1,13 @@
+# 250224 월 PM 6:23
+
 from collections import deque
 
 N, K = map(int, input().split())
-queue = deque(range(1, N+1))
+q = deque(range(1, N+1))
 result = []
 
-while queue:
-    queue.rotate(-(K-1))  # K-1번 왼쪽으로 회전
-    result.append(queue.popleft())  # K번째 요소 제거
+while q:
+    q.rotate(-(K-1))
+    result.append(q.popleft())
 
-print(f'<{", ".join(map(str, result))}>')
+print(f'<{', '.join(map(str, result))}>')
