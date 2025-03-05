d31a80888bc8bd5e895646a97905ba0cc835a1c9
Suhyeon Lee <78308684+limeorange@users.noreply.github.com>
2025-03-05 18:13:24 +0900

    [Gold V] Title: 강의실 배정, Time: 320 ms, Memory: 76548 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -0,0 +1,19 @@
+# 250305 수 PM 6:13 / 우선순위 큐 => 힙 자료구조 활용
+
+import sys
+import heapq
+input = sys.stdin.readline
+
+N = int(input())
+lst = []
+for _ in range(N):
+    s, e = map(int, input().split())
+    lst.append((s, e))
+lst.sort(key = lambda x:(x[0], x[1]))
+
+heap = [lst[0][1]]
+for s, e in lst[1:]:
+    if s >= heap[0]:
+        heapq.heappop(heap)
+    heapq.heappush(heap, e)
+print(len(heap))
