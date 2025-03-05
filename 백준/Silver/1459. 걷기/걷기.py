d045990e25f14e4d378355d2aac06a16b8d3628b
Suhyeon Lee <78308684+limeorange@users.noreply.github.com>
2025-03-05 19:48:21 +0900

    [Silver III] Title: 걷기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1459. 걷기/걷기.py
+++ 백준/Silver/1459. 걷기/걷기.py
@@ -1,17 +1,20 @@
+# 250305 수 PM 7:47
+
+import sys
+input = sys.stdin.readline
+
 X, Y, A, B = map(int, input().split())
 
-# 작은 값 m, 큰 값 M
 m = min(X, Y)
 M = max(X, Y)
 
-# 가능한 경우의 최소 비용 계산
+# 1) 직선 / 2) 대각선+직선 
 cost = min((X + Y) * A, m * B + (M - m) * A)
 
-# 대각선 이동이 더 이득일 때 처리
+# 3) 대각선 이동이 더 이득인 경우 (대각선, 대각선+직선)
 if B < 2 * A:
-    if (M - m) % 2 == 0:
+    if (M - m) % 2 == 0: # 대각선 M번 이동
         cost = min(cost, M * B)
-    else:
+    else: # 대각선 M-1번, 직선 1번 이동
         cost = min(cost, (M - 1) * B + A)
-
 print(cost)
