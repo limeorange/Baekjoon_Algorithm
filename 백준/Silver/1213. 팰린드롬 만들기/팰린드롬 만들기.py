624f2eb6a1b686b421d99816a9eba11cf94bd476
Suhyeon Lee <78308684+limeorange@users.noreply.github.com>
2025-03-08 18:36:50 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 56 ms, Memory: 34908 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.py
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.py
@@ -1,28 +1,29 @@
-# 250308 토 PM 6:29 / Counter의 key는 자동으로 정렬되지 않음!
+# 250308 토 PM 6:35
+# 성능 최적화 => 1) half => 리스트 append 후 ''.join() / 2) 함수로 만들기
 
 import sys
 from collections import Counter
 input = sys.stdin.readline
 
-data = input().strip()
+def make_palindrome(data):
 
-# 팰린드롬 가능 여부 => unique 문자 1) 짝수 n개 / 2) 홀수 1개, 짝수 n-1개
-counter = Counter(data)
-h_cnt = 0
-h_char = ''
-for c in counter:
-    if counter[c] % 2 == 1:
-        h_cnt += 1
-        h_char = c
-        counter[c] -= 1 # 팰린드롬의 중심에 홀수개에 해당하는 문자가 놓일 예정
-    if h_cnt == 2:
-        print('I\'m Sorry Hansoo')
-        break
+    # 팰린드롬 가능 여부 => unique 문자 1) 짝수 n개 / 2) 홀수 1개, 짝수 n-1개
+    counter = Counter(data)
+    h_cnt = 0
+    h_char = ''
+    for c in counter:
+        if counter[c] % 2 == 1:
+            h_cnt += 1
+            h_char = c
+            counter[c] -= 1 # 팰린드롬의 중심에 홀수개에 해당하는 문자가 놓일 예정
+        if h_cnt == 2:
+            return("I'm Sorry Hansoo")
 
-# 팰린드롬 만들기
-if h_cnt != 2:
-    half = ''
+    # 팰린드롬 만들기
+    half = []
     for c in sorted(counter.keys()): # Counter key 기준 '정렬'한 후 하나씩 순회해야 함
-        half += c * (counter[c]//2)
-    result = half + h_char + half[::-1]
-    print(result)
+        half.append(c * (counter[c]//2))
+    return ''.join(half) + h_char + ''.join(reversed(half))
+
+data = input().strip()
+print(make_palindrome(data))
