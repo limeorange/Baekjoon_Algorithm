ab5f9413b3f3435be8a79bea363d8d150d1759da
Suhyeon Lee <78308684+limeorange@users.noreply.github.com>
2025-03-08 19:53:35 +0900

    [Silver III] Title: 파일 정리, Time: 160 ms, Memory: 41456 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.py
+++ 백준/Silver/20291. 파일 정리/파일 정리.py
@@ -1,14 +1,16 @@
-# 250308 토 PM 7:43
+# 250308 토 PM 7:53 / name 정보는 변수에 저장할 필요없음, 기본 dict 사용해서 카운팅
 
 import sys
-from collections import defaultdict
 input = sys.stdin.readline
 
 N = int(input())
-dic = defaultdict(int)
+dic = dict()
 for _ in range(N):
-    name, ex = map(str, input().strip().rsplit('.', 1))
-    dic[ex] += 1
-
+    ex = input().strip().split('.').pop()
+    if ex in dic:
+        dic[ex] += 1
+    else:
+        dic[ex] = 1
+    
 for ex in sorted(dic):
     print(f'{ex} {dic[ex]}')
