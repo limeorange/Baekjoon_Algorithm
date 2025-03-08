620382370ee8a71cc1d65f383164a000ef374c9b
Suhyeon Lee <78308684+limeorange@users.noreply.github.com>
2025-03-08 18:20:52 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 60 ms, Memory: 35056 KB -BaekjoonHub

---
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.py
@@ -0,0 +1,29 @@
+# 250308 토 PM 6:20 / Counter의 key는 자동으로 정렬되지 않음!
+
+import sys
+from collections import Counter
+input = sys.stdin.readline
+
+data = input().strip()
+
+# 팰린드롬 가능 여부 => unique 문자 1) 짝수 n개 / 2) 홀수 1개, 짝수 n-1개
+counter = Counter(data)
+h_cnt = 0
+h_char = ''
+for c in counter:
+    if counter[c] % 2 == 1:
+        h_cnt += 1
+        h_char = c
+        counter[c] -= 1 # 팰린드롬의 중심에 홀수개에 해당하는 문자가 놓일 예정
+    if h_cnt == 2:
+        print('I\'m Sorry Hansoo')
+        break
+
+# 팰린드롬 만들기
+half = ''
+for c in sorted(counter.keys()): # Counter key 기준 '정렬'한 후 하나씩 순회해야 함
+    half += c * (counter[c]//2)
+result = half + h_char + half[::-1]
+
+if h_cnt != 2:
+    print(result)
